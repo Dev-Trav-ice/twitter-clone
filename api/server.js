b2ce82070e7155bb3f912d57dffb679119b49d85
Dennis Muganda <butunyidennis@gmail.com>
2025-05-05 12:38:47 +0300

    last

--- api/server.js
+++ api/server.js
@@ -22,7 +22,10 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5000", process.env.FRONTEND_URL],
+    origin: [
+      "http://localhost:5173",
+      "https://twitter-clone-xntj.onrender.com",
+    ],
     credentials: true,
   })
 );
